63360c4f4cca20271799804c34aff5a022c9128f
prince <prince18pat@gmail.com>
2019-09-01 20:46:37 +0900

    initial project created

---
+++ ios2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios2048
+//
+//  Created by Prince Kumar on 2019/09/01.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
