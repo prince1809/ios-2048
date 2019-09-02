f255b984e58e369be86c12eefd47f85318d5ac64
prince <prince18pat@gmail.com>
2019-09-03 01:22:57 +0900

    add tiles view animation

--- ios2048/Views/GameboardView.swift
+++ ios2048/Views/GameboardView.swift
@@ -17,6 +17,18 @@ class GameboardView: UIView {
     
     let provider = AppearanceProvider()
     
+    let tilePopStartScale: CGFloat = 0.1
+    let tilePopMaxScale: CGFloat = 1.1
+    let tilePopDelay: TimeInterval = 0.05
+    let tileExpandTime: TimeInterval = 0.18
+    let tileContractTime: TimeInterval = 0.08
+    
+    let tileMergeStartScale: CGFloat = 1.0
+    let tileMergeExpandTime: TimeInterval = 0.08
+    let tileMergeContractTime: TimeInterval = 0.08
+    
+    let perSequareSlideDuration: TimeInterval = 0.08
+    
     init(dimension d: Int, tileWidth width: CGFloat, tilePadding padding: CGFloat, cornerRadius radius: CGFloat, backgroundColor: UIColor, foregroundColor: UIColor) {
         assert(d > 0)
         dimension = d
@@ -61,7 +73,21 @@ class GameboardView: UIView {
         let y = tilePadding + CGFloat(row)*(tileWidth + tilePadding)
         let r = (cornerRadius >= 2) ? cornerRadius - 2 : 0
         let tile = TileView(position: CGPoint(x: x, y: y), width: tileWidth, value: value, radius: r, delegate: provider)
+        tile.layer.setAffineTransform(CGAffineTransform(scaleX: tilePopStartScale, y: tilePopStartScale))
         
         addSubview(tile)
+        bringSubviewToFront(tile)
+        tiles[IndexPath(row: row, section: col)] = tile
+        
+        // Add to board
+        UIView.animate(withDuration: tileExpandTime, delay: tilePopDelay, options: UIView.AnimationOptions(), animations: {
+            // Make the tile 'pop'
+            tile.layer.setAffineTransform(CGAffineTransform(scaleX: self.tilePopMaxScale, y: self.tilePopMaxScale))
+        }, completion: { finished in
+            // Shrink the tile after it 'pop'
+            UIView.animate(withDuration: self.tileContractTime, animations: { () -> Void in
+                tile.layer.setAffineTransform(CGAffineTransform.identity)
+            })
+        })
     }
 }
